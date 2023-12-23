2f09cad041e79c3053dac3cc81fe8f707279c389
prajakta1003 <contact.prajaktajamdar@gmail.com>
2023-12-23 18:16:25 +0530

    commit

--- models/userModel.js
+++ models/userModel.js
@@ -15,6 +15,11 @@ const userSchema = new Schema({
         type : String,
         required : true,
     },
+    type: {
+        type : String,
+        required: true,
+        default: "user",
+    }
     
 })
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { addProduct, deleteProduct } = require("../controller/productController");
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
 
 const router = express.Router();
 
-router.post("/add-product", addProduct)
+router.post("/add-product",auth,admin, addProduct)
 router.delete("/delete/:productId", deleteProduct)
 
 
